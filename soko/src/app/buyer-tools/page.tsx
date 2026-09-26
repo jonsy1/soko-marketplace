6bacddc377ad386e56a0ba3c0c8dbd3aab321386
jonson jackson <jonson.jackson06@gmail.com>
2026-09-26 09:21:16 +0300

    Enable Wishlist tool

--- soko/src/app/buyer-tools/page.tsx
+++ soko/src/app/buyer-tools/page.tsx
@@ -39,7 +39,7 @@ const TOOLS = [
         <path d="M20.8 4.6a5.5 5.5 0 0 0-7.8 0L12 5.6l-1-1a5.5 5.5 0 0 0-7.8 7.8l1 1L12 21l7.8-7.6 1-1a5.5 5.5 0 0 0 0-7.8z" />
       </svg>
     ),
-    available: false,
+    available: true,
   },
   {
     href: '/buyer-tools/delivery-estimator',
